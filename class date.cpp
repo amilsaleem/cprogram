a168ca81229228d9fbeea1aa0b15b23932d282e5
amilsaleem <31254366+amilsaleem@users.noreply.github.com>
2017-08-25 21:32:16 +0530

    age finder
    program in  c++ to find your age geting value in function

--- class date.cpp
+++ class date.cpp
@@ -3,27 +3,66 @@ using namespace std;
 class Date
 {
 	int date,month,year;
-	public:
-		void set(int &a,int &b,  int &c);
-		void output()
-		{
-			cout<<date<<"/"<<month<<"/"<<year;
-		}
-};
-void Date::set(int &a,int &b,int &c)
-{
-	date=a;
-	month=b;
-	year=c;
+    
 	
+	public:
+	
+			
+		void set(int &a,int &b,int &c,int &d,int &e,int &f);
+	
+		void result();
+};
+
+
+
+void Date::set(int &a,int &b,int &c,int &d,int &e,int &f)
+{
+	if(a>d&&b>e)
+	{
+	
+	date=d+30-a;
+	month=(e+11)-b;
+	year=f-1-c;
+}
+else if(b>e)
+{
+	date=c-a;
+	month=(e+12) -b;
+	year=(f-1)-c;
+	
+}
+else if(a>d)
+{
+	date=(d+30)-a;
+	month=(e-1)-b;
+	year=f-c;
+	
+}
+else
+{
+	date=d-a;
+	month=e-b;
+	year=f-c;
+}
+}
+void Date::result()
+{
+cout<<date<<"day"<<month<<"month"<<year<<"year";
 }
 int main()
 {
-	Date d;
+	Date dob;
 	int a,b,c;
-	cout<<"Enter Date";
+	int d,e,f;
+	
+	cout<<"Enter Date of birth";
 	cin>>a>>b>>c;
-	d.set(a,b,c);
-	d.output();
+	cout<<"Enter today date";
+	cin>>d>>e>>f;
+
+	dob.set(a,b,c,d,e,f);
+	
+
+	dob.result();
 	
 }
